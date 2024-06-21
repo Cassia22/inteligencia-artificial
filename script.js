e63ca239954ca6fffd5e2a13069ddc738e91295c
cassia <105526219+Cassia22@users.noreply.github.com>
2024-06-21 19:21:57 -0300

    Add files via upload
    21/06

---
+++ script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const pergunta = [
+    enunciado: "Pergunta1",
+    alternativas: [
+        "Alternativa1",
+        "Alternativa2"]
+
+    ,
+
+
+    "Pergunta2",
+]
